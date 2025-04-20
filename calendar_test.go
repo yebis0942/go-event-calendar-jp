1aa035617c0671aa4d83862b480e50ba16c615a7
yebis0942 <yebis0942@gmail.com>
2025-04-20 16:11:44 +0900

    Add calendar_test.go

---
+++ calendar_test.go
@@ -0,0 +1,46 @@
+package gojpcal_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	gojpcal "github.com/yebis0942/golang-jp-event-calendar"
+)
+
+func TestGenerateICalendar(t *testing.T) {
+	events := []gojpcal.Event{
+		{
+			ID:          1,
+			Title:       "Sample Event",
+			Description: "This is a sample event.",
+			StartTime:   time.Date(2023, 10, 1, 10, 0, 0, 0, time.UTC),
+			EndTime:     time.Date(2023, 10, 1, 12, 0, 0, 0, time.UTC),
+			URL:         "http://example.com",
+			Place:       "Sample Place",
+			Address:     "123 Sample Street",
+		},
+	}
+
+	got, err := gojpcal.GenerateICalendar(events)
+	require.NoError(t, err)
+
+	want := "BEGIN:VCALENDAR\r\n" +
+		"VERSION:2.0\r\n" +
+		"PRODID:github.com/yebis0942/golang-jp-event-calendar\r\n" +
+		"CALSCALE:GREGORIAN\r\n" +
+		"METHOD:PUBLISH\r\n" +
+		"X-WR-CALNAME;VALUE=TEXT:Goコミュニティのイベント\r\n" +
+		"BEGIN:VEVENT\r\n" +
+		"UID:connpass-1@golang-jp-event-calendar.yebis0942.workers.dev\r\n" +
+		"DTSTART:20231001T100000Z\r\n" +
+		"DTEND:20231001T120000Z\r\n" +
+		"DTSTAMP:" + time.Now().UTC().Format("20060102T150405Z") + "\r\n" +
+		"SUMMARY:Sample Event\r\n" +
+		"DESCRIPTION:This is a sample event.\\n\\nURL: http://example.com\r\n" +
+		"LOCATION:Sample Place\\, 123 Sample Street\r\n" +
+		"URL:http://example.com\r\n" +
+		"END:VEVENT\r\n" +
+		"END:VCALENDAR\r\n"
+	require.Equal(t, want, got)
+}
